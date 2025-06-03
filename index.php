67e120bdfdb301ef3ee2e63bfc2f764bde6c93e0
Vitor Hugo <mvitorhugo862@gmail.com>
2025-06-03 15:03:14 -0300

    salvar

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
@@ -10,7 +10,8 @@
         integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
+    <link
+        href="https://fonts.googleapis.com/css2?family=DM+Serif+Display:ital@0;1&family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap"
         rel="stylesheet">
 </head>
 
@@ -54,15 +55,37 @@
 
 
     <main>
+       <?php
+   
+
+   $pagina = $_GET["param"] ?? "home";
+
+   $pagina = "paginas/{$pagina}.php";
+
+   if (file_exists($pagina)) {
+       include $pagina;
+   } else {
+       include "paginas/erro.php";
+   }
+    ?>
 
 
     </main>
 
 
     <footer>
+        <p>
+            <img src="imagens/barbearialogo.png" alt="Logo">Barbearia LK
+        </p>
+        <p>
+            (44) 99869-1352
+            <Br>
+            Avenida Julio José de Souza, Alto Palmital, Boa Esperança
+            <br>
 
+        </p>
     </footer>
-
+    
 </body>
 
 </html>
