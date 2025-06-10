d5271bf44548e9ddb0a6eb5902bfb3fb1d29b9e3
Vitor Hugo <mvitorhugo862@gmail.com>
2025-06-10 19:36:54 -0300

    Crie o array dos produtos (cortes)

---
+++ paginas/array.php
@@ -0,0 +1,13 @@
+<?php
+
+    $cortes = [
+        ["nome" => "Social", "valor" => 25],
+        ["mome" => "Degrade", "valor" => 30],
+        ["nome" => "Barba", "valor" => 15],
+        ["nome" => "Platinado", => 100 ],
+        ["nome" => "Luzes", => "valor" => 80],
+        ["nome" => "Sombrancelha" => "valor" => 5]
+    
+    ]
+
+?>
---
+++ paginas/galeria.php
@@ -0,0 +1,6 @@
+<div class="container">
+    <div class="galeria">
+        <h2 class="h2"> Galeria </h2>
+        
+    </div>
+</div>
